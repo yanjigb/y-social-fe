c8dba03c0da8fa22fad075536ed94abfa18c07c0
yanjigb <yanji.notification@gmail.com>
2024-11-09 22:28:46 +0700

    feat(ui): :sparkles: [#8] Add Format MDX Advertise Card

--- src/components/features/app-advertise/components/sponsored-card/index.jsx
+++ src/components/features/app-advertise/components/sponsored-card/index.jsx
@@ -2,16 +2,20 @@
 import { useIntersectionObserver } from "@uidotdev/usehooks";
 import SponsoredBadge from "components/ui/sponsored-badge";
 import { ADVERTISE_STATUS } from "constant/advertise";
+import { CheckCircle2 } from "lucide-react";
 import { memo, useEffect, useState } from "react";
 import isEqual from "react-fast-compare";
+import ReactMarkdown from "react-markdown";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   handleUpdateClicks,
   handleUpdateImpressions,
 } from "redux/request/advertiseRequest";
+import { getUserByID } from "redux/request/userRequest";
 import Lightbox from "yet-another-react-lightbox";
-import { Photo } from "../../../../ui";
+import useCurrentUser from "../../../../../hooks/useCurrentUser";
+import { Avatar, Photo } from "../../../../ui";
 import Skeleton from "./skeleton";
 
 /* eslint-disable react/react-in-jsx-scope */
@@ -23,12 +27,19 @@ const SponsoredCard = ({
   media_content,
   link_action,
   status,
+  userID,
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const fullDescription = description || "";
   const [openPreviewImage, setOpenPreviewImage] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [user, setUser] = useState({
+    _id: "",
+    username: "",
+    profilePicture: "",
+    isVerify: false,
+  });
+  const currentUser = useCurrentUser();
 
   const handleClickAdvertise = async () => {
     const result = await handleUpdateClicks(id, dispatch);
@@ -68,12 +79,37 @@ const SponsoredCard = ({
 
   if (!title || !description || !cta) return <Skeleton />;
 
-  const truncatedDescription = description.slice(0, 100) + "...";
+  if (
+    status === ADVERTISE_STATUS.SUSPENDED ||
+    status === ADVERTISE_STATUS.SCHEDULE
+  ) {
+    return null;
+  }
 
-  if(status === ADVERTISE_STATUS.SUSPENDED 
-    || status === ADVERTISE_STATUS.SCHEDULE) {
-      return null;
-    }
+  useEffect(() => {
+    let isCancelled = false;
+
+    getUserByID(userID, dispatch)
+      .then((data) => {
+        if (!isCancelled) {
+          const { _id, username, profilePicture, isVerify } = data?.user || {};
+
+          setUser({
+            _id: _id,
+            username: username,
+            profilePicture: profilePicture,
+            isVerify: isVerify,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error("Failed", err);
+      });
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [userID, dispatch]);
 
   return (
     <>
@@ -81,8 +117,42 @@ const SponsoredCard = ({
         ref={ref}
         className="post gap-4 w-full mx-auto overflow-hidden rounded-xl shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out"
       >
+        <div
+          className="user"
+          title={
+            user?._id === currentUser?._id
+              ? `Truy cập trang cá nhân`
+              : `Truy cập trang cá nhân ${user.username}`
+          }
+        >
+          <Link
+            to={`/user/${user?._id}`}
+            className="profile-pic bg-black text-white fs-5"
+            aria-label="Avatar user"
+          >
+            <Avatar
+              imageSrc={user.profilePicture}
+              label={user.username}
+              userId={user?._id}
+            />
+          </Link>
+          <Link to={`/user/${user?._id}`} className="info">
+            <div className="d-flex align-items-center fs-5">
+              <div className="fw-bold d-flex align-items-center">
+                {user.username || "loading..."}
+                {user.isVerify && (
+                  <CheckCircle2 size={14} className="ms-2 text-primary" />
+                )}
+              </div>
+            </div>
+            <span>
+              <>@{user?.username || "loading..."}</>
+            </span>
+          </Link>
+        </div>
+
         <button
-          className="-mt-3 w-full object-cover rounded-xl"
+          className="w-full object-cover rounded-xl"
           onClick={onOpenPreviewImage}
         >
           <Photo
@@ -99,10 +169,16 @@ const SponsoredCard = ({
             <SponsoredBadge />
             <div className="text-2xl flex items-start flex-col mt-4">
               {description.length < 100 ? (
-                <p>{description}</p>
+                <ReactMarkdown className="prose prose-lg">
+                  {description}
+                </ReactMarkdown>
               ) : (
                 <>
-                  <p>{isExpanded ? fullDescription : truncatedDescription}</p>
+                  <ReactMarkdown className="prose prose-lg">
+                    {isExpanded
+                      ? description
+                      : description.slice(0, 100) + "..."}
+                  </ReactMarkdown>
                   <button
                     onClick={() => setIsExpanded(!isExpanded)}
                     className="text-blue-500 hover:text-blue-700 font-medium mt-2 focus:outline-none"
